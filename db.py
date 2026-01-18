43ef732412f31c7c8bf2c0c56a56372396b32c4a
markiyan2009 <93789450+markiyan2009@users.noreply.github.com>
2026-01-18 11:59:02 +0200

    Init

---
+++ db.py
@@ -0,0 +1,153 @@
+import asyncio
+import logging
+import sys
+from token_api import TOKEN_API
+from os import getenv
+from aiogram import Bot, Dispatcher, html, Router, F
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart, Command
+from aiogram import types
+import os
+import supabase
+from random import randint, shuffle
+
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
+
+class WordState(StatesGroup):
+    waiting_translation = State()
+    word_id = State()
+    waiting_word = State()
+    waiting_explenation = State()
+    count_words = State()
+    count_questions = State()
+
+db_router = Router(name='db')
+url = 'https://strmitiqeerzvavfdzag.supabase.co'
+key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0cm1pdGlxZWVyenZhdmZkemFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM3NDg2OTEsImV4cCI6MjA3OTMyNDY5MX0.HU6OgY-a21mmqxNk0_k67kwuBVU3zrSUtMqR6sOtEa4'
+
+sp_db = supabase.create_client(url,key)
+from sentence_transformers import SentenceTransformer
+model = SentenceTransformer("intfloat/multilingual-e5-large")
+
+
+
+print("model loaded")
+
+
+from sklearn.metrics.pairwise import cosine_similarity
+
+def add_word_data(word):
+    
+    try:
+        word_emb = model.encode(word).tolist()
+        response = sp_db.table('words').insert({'word':word, "embedding":word_emb}).execute()
+        return response.data[0]["id"]
+        
+    except Exception as exception:
+        return exception
+
+def get_word_data(word=None, id = None):
+    try:
+        if id:
+            response = sp_db.table("words").select('*').eq("id",id).execute()
+        else:
+            response = sp_db.table("words").select('*').eq("word",word).execute()
+        return response
+    except Exception as exception:
+        return exception
+
+def add_trans_data(trans, word_id, expl = None):
+    try:
+        trans_emb = model.encode(trans).tolist()
+        response = sp_db.table('translation').insert({'trans':trans, 'word_id':word_id, "embedding":trans_emb,"explenation":expl}).execute()
+        return response
+        
+    except Exception as exception:
+        return exception
+
+def get_trans_data(word):
+    try:
+
+        response = sp_db.table("translation").select('*').eq("trans",word).execute()
+        return response
+    except Exception as exception:
+        return exception
+    
+
+def get_all_translations():
+    return sp_db.table("translation").select("trans, embedding, explenation").execute().data
+
+def find_similar(trans_word, min_sim=0.7, max_sim=0.98):
+    result = []
+
+    base = get_trans_data(trans_word).data
+    if not base:
+        return []
+
+    base_emb = base[0]["embedding"]
+
+    for t in get_all_translations():
+        if t["trans"] == trans_word:
+            continue
+
+        sim = cosine_similarity(
+            [base_emb],
+            [t["embedding"]]
+        )[0][0]
+
+        if min_sim < sim < max_sim:
+            result.append({
+                "trans": t["trans"],
+                "explenation": t["explenation"],
+                
+            })
+
+    
+    return result[:3]
+
+def create_test(questions_count = 10):
+    result = []
+    for i in range(questions_count):    
+        test = []
+        all_trans = get_all_translations()
+        question_obj = get_trans_data(all_trans[randint(0,len(all_trans)-1)]["trans"]).data
+        question_trans = question_obj[0]["trans"]
+        question = get_word_data(id = question_obj[0]['word_id']).data[0]["word"]
+        answers_required = find_similar(question_trans)
+        
+        shuffle(answers_required)
+        answers = []
+        
+
+        for i in range(3):
+            # question_obj[0]["explenation"]
+            answers.append({
+                "text": answers_required[i]["trans"],
+                "correct": False,
+                "explenation": answers_required[i]["explenation"]
+            })
+        
+        corect_expl = question_obj[0]['explenation']
+        answers.append({
+            "text": question_trans,
+            "correct" : True,
+            "explenation":  corect_expl
+            })
+        
+        shuffle(answers)
+        test.append(question)
+        test.append(answers)
+        result.append(test)
+    return result
+        
+        
+        
+        
+        
+
+
+#перевірити find_similar, 4 відповіді в тесті, чергування питань англійських і українських слів
+
---
+++ main.py
@@ -0,0 +1,113 @@
+from db import *
+# Bot token can be obtained via https://t.me/BotFather
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+# All handlers should be attached to the Router (or Dispatcher)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def command_start_handler(message: types.Message) -> None:
+
+    kb = [[ types.KeyboardButton(text="Записати слово"), types.KeyboardButton(text="Згенерувати тест")],]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True, input_field_placeholder="бойко")
+    await message.answer("Виберіть дію", reply_markup=keyboard)
+
+
+@dp.message(F.text == 'Записати слово')
+async def get_word_state(message: types.Message, state: FSMContext) -> None:
+    try:
+        await message.answer('Напишіть слово')
+        await state.set_state(WordState.waiting_word)
+        
+    except TypeError:
+        await message.answer("Nice try!")
+
+@dp.message(WordState.waiting_word)
+async def get_word(message: types.Message, state: FSMContext):
+    word = message.text
+    # запис у базу...
+    word_id = add_word_data(word)
+    await state.update_data(word_id=word_id)
+    await message.answer(f"Слово '{word}' записано!")
+    
+    await message.answer('Запишіть переклад')
+    await state.set_state(WordState.waiting_translation)
+
+@dp.message(WordState.waiting_translation)
+async def get_translation(message: types.Message, state: FSMContext):
+    translation = message.text
+    # запис у базу...
+    data = await state.get_data()
+    word_id = int(data.get("word_id"))
+    await state.update_data(trans=translation)
+    await message.answer("Переклад записано")
+    await state.set_state(WordState.waiting_explenation)
+    await message.answer("Запишіть пояснення(якщо його немає запишіть 0)")
+
+@dp.message(WordState.waiting_explenation)
+async def get_explenation(message: types.Message,state:FSMContext):
+    explenation = message.text
+    data = await state.get_data()
+    word_id = int(data.get("word_id"))
+    trans = str(data.get("trans"))
+    if explenation == "0":
+
+        add_trans_data(trans,word_id,None)
+    else:
+        add_trans_data(trans,word_id,explenation)
+    await message.answer("Переклад і пояснення записано")
+    await state.clear()
+    
+@dp.message(F.text == "Згенерувати тест")
+async def count_questions(message:types.Message,state: FSMContext):
+    try:
+        await message.answer('Скільки питань?')
+        await state.set_state(WordState.count_questions)
+        
+    except TypeError:
+        await message.answer("Nice try!")
+
+@dp.message(WordState.count_questions)
+async def test(message: types.Message, state: FSMContext):
+    try:
+        count = int(message.text)
+    except ValueError:
+        await message.answer("Введіть число!")
+        return
+    
+    tests_list = create_test(count)  # повинен повертати список питань та відповідей
+    
+    for test_item in tests_list:
+        question = test_item[0]
+        answers = test_item[1]  # список словників: [{'text': ..., 'correct': True/False, 'explanation': ...}, ...]
+        
+        # формуємо список текстів відповідей
+        poll_options = [f"{a['text']}" for a in answers]
+        
+        # визначаємо правильну відповідь
+        try:
+            correct_option_id = next(i for i, a in enumerate(answers) if a['correct'])
+        except StopIteration:
+            correct_option_id = 0  # якщо випадково нема правильної відповіді
+        print(answers[correct_option_id])
+        await message.answer_poll(
+            question=question,
+            options=poll_options,
+            type="quiz",
+            correct_option_id=correct_option_id,
+            is_anonymous=False,
+            explanation="Повний варіант: " + answers[correct_option_id]['explenation'] if answers[correct_option_id]['explenation'] is not None else None
+        )
+        
+    
+    await state.clear()
+
+async def main() -> None:
+
+    bot = Bot(token=TOKEN_API, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    
+    # And the run events dispatching
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
